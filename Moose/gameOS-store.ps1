0c26b5cc072f584a2441b11b48b097906f4f02fa
NosoTriso <150440947+NosoTriso@users.noreply.github.com>
2023-11-12 17:30:52 +0000

    Create gameOS-store.ps1

---
+++ Moose/gameOS-store.ps1
@@ -0,0 +1,18 @@
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
+
+$DownloadURL = 'https://raw.githubusercontent.com/MooseSoftware/gameOS/main/gameos.bat'
+
+$FilePath = "$env:TEMP\store.bat"
+
+try {
+    Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing -OutFile $FilePath
+} catch {
+    Write-Error $_
+	Return
+}
+
+if (Test-Path $FilePath) {
+    Start-Process $FilePath -Wait
+    $item = Get-Item -LiteralPath $FilePath
+    $item.Delete()
+}
